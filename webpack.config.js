b3c3a57e1b451c5b47abc16ff14026a3d3cfd98e
l-palermo <l-palermo@hotmail.it>
2020-06-06 16:23:18 +0100

    add react, webpack plugins, refactor

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = (env) => {
   const environment = env ? "production" : "development";
@@ -6,14 +8,24 @@ module.exports = (env) => {
   return {
     mode: environment,
     // entry point from which webpack loads all the modules included in the app.
-    entry: "./src/index.js",
+    entry: {
+      // provision to have multiple entry points just add them as attributes or the entry object
+      app: "./src/index.js",
+    },
     // where webpack will save the bundle
     output: {
-      filename: "main.js",
+      // this allows webpac k to create a bundle for each entry point. In this case it would be 'app.bundle.js'
+      filename: "[name].bundle.js",
       path: path.resolve(__dirname, "dist"),
     },
     module: {
       rules: [
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          loader: "babel-loader",
+          options: { presets: ["@babel/env", "@babel/preset-react"] },
+        },
         {
           test: /\.css$/,
           use: ["style-loader", "css-loader"],
@@ -31,5 +43,9 @@ module.exports = (env) => {
       contentBase: "./dist",
       port: 3000,
     },
+    plugins: [
+      new CleanWebpackPlugin(),
+      new HtmlWebpackPlugin({ template: "./public/index.html" }),
+    ],
   };
 };
